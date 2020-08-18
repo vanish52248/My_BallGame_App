f644c4b420740d954faa56396dab3419ad88e664
L <62473696+vanish52248@users.noreply.github.com>
2020-08-18 23:11:55 +0900

    Add files via upload

---
+++ StartActivity.java
@@ -0,0 +1,22 @@
+package com.example.catchofball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.codingwithsara.catchtheball.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void startGame(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+}
